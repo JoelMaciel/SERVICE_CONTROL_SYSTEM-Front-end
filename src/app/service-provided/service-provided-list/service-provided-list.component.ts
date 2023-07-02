17106c31c3d2b995e54b5571336a13cb62ed9e6c
Joel Maciel <soldado25461@gmail.com>
2023-07-01 23:00:16 -0300

    feat: creat search form

--- src/app/service-provided/service-provided-list/service-provided-list.component.ts
+++ src/app/service-provided/service-provided-list/service-provided-list.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-service-provided-list',
   templateUrl: './service-provided-list.component.html',
-  styleUrls: ['./service-provided-list.component.css']
+  styleUrls: ['./service-provided-list.component.css'],
 })
 export class ServiceProvidedListComponent implements OnInit {
+  name: string;
+  month: number;
+  months: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  search() {
+    console.log(this.name, this.month);
   }
-
 }
